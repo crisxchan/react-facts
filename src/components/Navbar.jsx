ed970d915ef88b41e407dbfd6a7fd08943369c23
crisxchan <chrisgonzales.online@gmail.com>
2022-07-26 21:05:38 +0800

    Initial commit w nav css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import ReactIcon from '../assets/react.svg'
+
+export default function Navbar() {
+    return (
+        <header>
+            <img src={ReactIcon} alt="react logo"/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </header>
+    )
+}
