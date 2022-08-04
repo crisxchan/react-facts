7dd1162d0b02db011d546f1f87aa0748183887f0
crisxchan <chrisgonzales.online@gmail.com>
2022-08-04 14:30:54 +0800

    Refactor code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 import ReactIcon from '../assets/react.svg'
 
 export default function Navbar(props) {
-    console.log(props.darkMode)
     return (
         <header className={props.darkMode ? "dark" : "light"}>
             <img src={ReactIcon} alt="react logo"/>
             <h3>ReactFacts</h3>
-            <div id='toggler' onClick={props.darkModeHandler}>
-                <p id='toggler-dark'  className='toggler-label'>Dark</p>
+            <div id="toggler" onClick={props.darkModeHandler}>
+                <p id="toggler-dark"  className="toggler-label">Dark</p>
                 <div id="toggler-container">
-                    <div id='toggler-circle'></div>
+                    <div id="toggler-circle"></div>
                 </div>
-                <p id='toggler-light'  className='toggler-label'>Light</p>
+                <p id="toggler-light"  className="toggler-label">Light</p>
             </div>
         </header>
     )
