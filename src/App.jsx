c9b74757dfe8f06ab841afc0fdfe1ddf01b31a8e
crisxchan <chrisgonzales.online@gmail.com>
2022-08-04 13:34:09 +0800

    Complete light/dark mode for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import Main from './components/Main'
 import Navbar from './components/Navbar'
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(true)
+
+  function toggleMode() {
+      setDarkMode(prevDarkMode => !prevDarkMode)
+  }
+
   return (
     <>
-      <Navbar />
+      <Navbar darkMode={darkMode} darkModeHandler={toggleMode}/>
       <Main />
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import ReactIcon from '../assets/react.svg'
 
-export default function Navbar() {
+export default function Navbar(props) {
+    console.log(props.darkMode)
     return (
-        <header>
+        <header className={props.darkMode ? "dark" : "light"}>
             <img src={ReactIcon} alt="react logo"/>
             <h3>ReactFacts</h3>
-            <div id='toggler'>
+            <div id='toggler' onClick={props.darkModeHandler}>
                 <p id='toggler-light'  className='toggler-label'>Light</p>
-                <div id='toggler-container'>
+                <div id="toggler-container">
                     <div id='toggler-circle'></div>
                 </div>
                 <p id='toggler-dark'  className='toggler-label'>Dark</p>
